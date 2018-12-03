3d591e6be21142e95039c09d54836402e7b9a351
CapitalistLepton <CapitalistLepton@users.noreply.github.com>
2018-12-03 10:25:31 -0800

    Zero out registers in zane test file.

--- zane.asm
+++ zane.asm
@@ -2,11 +2,14 @@
 ; Zane Littrell
 ;
 ; Tests the LD, JMP, AND, ADD, NOT, and ST instructions
-; Should have 9 stored at address x000A at the end
+; Should have 9 stored at address x000D at the end
 ;
 	.orig x0000
-	LD	R0, ADDR	; Load x0004 in R0
-	JMP	R0		; Jump to x0004
+	AND	R0, R0, #0	; Zero out registers
+	AND	R1, R1, #0
+	AND	R2, R2, #0
+	LD	R0, ADDR	; Load x0007 in R0
+	JMP	R0		; Jump to x0007
 	AND	R0, R0, #0	; Should not be run
 	ADD	R1, R1, #5
 	ADD	R2, R2, #-9	; Put -9 into R2
@@ -15,6 +18,6 @@
 	ST	R2, DATA	; 9 stored into DATA
 	HALT
 
-	ADDR	.fill 4
+	ADDR	.fill 7		; address to jump to
 	DATA	.blkw 1
 	.end
